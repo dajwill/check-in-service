9993d9300567ab9a9cc1301ede51515b1d99ac81
David Williams <david.williams@webascender.com>
2016-03-22 14:53:57 -0400

    finishes model tests

--- spec/models/check_in_spec.rb
+++ spec/models/check_in_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
 describe CheckIn do
+  let(:business){
+    Business.create(name: 'test', code: '1111')
+  }
 
-  it 'can not be created without code' do
+  it 'can not be created without business code' do
     # check_in = create :check_in
     expect{create :check_in}.to raise_error
   end
 
-  it 'needs tests to be written!' do
-    pending('write tests for CheckIn!')
+  it 'can be created' do
+    check_in = CheckIn.create(user_id: 1, business_id: business.id, business_code: business.code)
+    expect(check_in).to_not be_nil
   end
 
 end
